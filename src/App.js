26e058ca185a6c1d768e1d7dd2fa6312a922b69a
Maverick <sc20ml@leeds.ac.uk>
2024-02-15 10:35:45 +0000

    Finished Simple To Do React App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useRef, useEffect} from 'react'
+import ToDoList from './ToDoList';
+import { v4 as uuidv4 } from 'uuid';
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [todos, setTodos] = useState([]);
+	const todoNameRef = useRef();
+
+	// Load to do items stored in local storage once on start up
+	useEffect( () => {
+		const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+
+		if(storedTodos) {
+			setTodos(prevTodos => [...storedTodos]);
+		}
+	
+	}, [])
+
+	// Save todos to local storage so it stays when page is refreshed
+	useEffect( () => {
+		localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+	}, [todos])
+
+	// Add an item to the to do list
+	function addToDo() {
+		const name = todoNameRef.current.value
+		setTodos(prevArray => {
+			return [...prevArray, {id: uuidv4(), name: name, complete: false}]
+		});
+
+		todoNameRef.current.value = null;
+	}
+
+
+	// Toggle completed checkbox
+	function toggleTodo(id) {
+		// Copy current to do list
+		const newTodos = [...todos];
+
+		// Find to do to check
+		const todo = newTodos.find(todo => todo.id === id);
+		todo.complete = !todo.complete;
+		setTodos(newTodos);
+	}
+
+	// Clear all
+	function clearAll() {
+		const newTodos = [];
+		setTodos(newTodos);
+	}
+
+	// Clear completed
+	function clearCompleted() {
+		const uncompletedTodos = todos.filter( todo => !todo.complete);
+		setTodos(uncompletedTodos);
+	}
+
+
+
+	return(
+		
+		<>	
+			< ToDoList todos = {todos} toggleTodo = {toggleTodo} />
+			<input ref={todoNameRef} type = "text" />
+			<button onClick = {addToDo}> Add To Do</button>
+			<button onClick = {clearCompleted}> Clear Complete </button>
+			<button onClick = {clearAll}> Clear All</button>
+			<div> {todos.filter(todo => !todo.complete).length}</div>
+		</>
+	)
 }
 
 export default App;
